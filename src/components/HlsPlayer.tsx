38eb6cfb553d5dac8b928e6f6bf7d166c2b5115a
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-05-21 09:37:25 +0800

    ✨feature: 新增 HlsPlayer

---
+++ src/components/HlsPlayer.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import Hls from "hls.js"
+import { MediaHTMLAttributes, forwardRef, useEffect, useImperativeHandle, useRef } from "react"
+
+export interface HlsPlayerProps extends Omit<MediaHTMLAttributes<HTMLVideoElement>, "src"> {
+    src: string
+}
+
+const HlsPlayer = forwardRef<HTMLVideoElement, HlsPlayerProps>((props, ref) => {
+    const { src, ...rest } = props
+    const video = useRef<HTMLVideoElement>(null)
+
+    useImperativeHandle(ref, () => video.current!, [])
+
+    useEffect(() => {
+        const { current: player } = video
+        if (!player || !src) return
+        if (player.canPlayType("application/vnd.apple.mpegurl")) {
+            player.src = src
+            player.play()
+            return
+        }
+        if (Hls.isSupported()) {
+            const hls = new Hls()
+            hls.loadSource(src)
+            hls.attachMedia(player)
+            player.play()
+            return () => {
+                hls.destroy()
+            }
+        }
+    }, [src])
+
+    return <video ref={video} {...rest} />
+})
+
+export default HlsPlayer
