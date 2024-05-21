672d1f94d8468f3d25dcb768dbba14a5808ad766
陆子虚 <11374106+zixulu@user.noreply.gitee.com>
2024-05-21 16:34:46 +0800

    🐞 fix: 解决了一些问题

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -4,11 +4,12 @@ import { css } from "@emotion/css"
 import { clsx } from "deepsea-tools"
 import { forwardRef, HTMLAttributes } from "react"
 
-const Skeleton = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(props => {
+const Skeleton = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>((props, ref) => {
     const { className, ...rest } = props
 
     return (
         <div
+            ref={ref}
             className={clsx(
                 css`
                     @keyframes shimmer {
